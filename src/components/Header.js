470de6512a97c05e5fc18f194b67248d0ed3dd3d
Alex Bejenaru <alex.d.bejenaru@gmail.com>
2022-01-17 10:40:46 +0200

    Added starter files

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return ( 
+        <>
+        <h1>hei</h1>
+        </>
+     );
+}
+ 
+export default Header;
---
+++ src/data/db.js
@@ -0,0 +1,11 @@
+const menu = [
+    {
+        id: 0,
+        title: 'Butermilk Pancakes',
+        category: 'shakes',
+        price: 15.90,
+        img: ''
+    }
+]
+
+export default menu;
